5b9a91d0a84b410b4145ae1292bbbb364463e38e
colinskeep <colinjinskeep@gmail.com>
2019-03-07 20:08:36 -0700

    twitter login

--- src/v1/controllers/account.js
+++ src/v1/controllers/account.js
@@ -1,5 +1,6 @@
 const jwt = require('../components/jwt.js');
 const request = require('request');
+const registrationModel = require('../models/registration.js');
 
 /**
  * Function to execute when endpoint reached
@@ -10,11 +11,12 @@ async function getToken(req, res) {
   try {
     const token = req.headers.authorization.split(' ')[1];
     const userObj = await jwt.resolve(token);
-    if (token) {
+    const userProfile = await registrationModel.findOne({email: userObj.email});
+    if (userObj && userProfile.password === userObj.password) {
       request.post({
         url: 'https://api.twitter.com/oauth/request_token',
         oauth: {
-          oauth_callback: 'http://localhost:9000/api/v1/',
+          oauth_callback: 'https://ozark-prod.herokuapp.com/',
           consumer_key: process.env.TWITTER_CONSUMER_KEY,
           consumer_secret: process.env.TWITTER_SECRET_KEY,
         },
@@ -22,6 +24,7 @@ async function getToken(req, res) {
         if (err) {
           return res.send(500, {message: e.message});
         }
+        console.log(body);
         var jsonStr = '{ "' + body.replace(/&/g, '", "').replace(/=/g, '": "') + '"}';
         console.log(jsonStr);
         res.send(JSON.parse(jsonStr));
@@ -32,6 +35,21 @@ async function getToken(req, res) {
   }
 }
 
+/**
+ * Function to execute when endpoint reached
+ * @param {string} req - incoming request
+ * @param {string} res - outgoing response
+ */
+async function getApiKeys(req, res) {
+  try {
+    console.log(req.params.oauthtoken);
+    console.log(req.params.oauth_verifier);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
 module.exports = {
   getToken,
+  getApiKeys,
 };
