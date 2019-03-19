d7af88c7eaa312bd2c4380ddbdb76888b84eeed0
colinskeep <colinjinskeep@gmail.com>
2019-03-18 21:12:06 -0700

    username is available to own user

--- src/v1/controllers/available.js
+++ src/v1/controllers/available.js
@@ -18,14 +18,13 @@ async function postAvailable(req, res) {
       if (userProfile.username == req.body.username) {
         res.status(200).json({nameAvailable: true, allowedWords: true});
       }
+    }
+    const searchResults = await available.userName(req.body.username);
+    if (searchResults && restrictedPath.indexOf(req.body.username.toLowerCase()) < 0) {
+      console.log(searchResults);
+      res.status(200).json(searchResults);
     } else {
-      const searchResults = await available.userName(req.body.username);
-      if (searchResults && restrictedPath.indexOf(req.body.username.toLowerCase()) < 0) {
-        console.log(searchResults);
-        res.status(200).json(searchResults);
-      } else {
-        res.status(200).json({nameAvailable: false, allowedWords: false});
-      }
+      res.status(200).json({nameAvailable: false, allowedWords: false});
     }
   } catch (err) {
     // eslint-disable-next-line no-console
