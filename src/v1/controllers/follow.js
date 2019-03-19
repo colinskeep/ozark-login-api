df88967ce9fbe539515f7a5283c285c9703b8b86
colinskeep <colinjinskeep@gmail.com>
2019-03-18 23:18:39 -0700

    remove logs

--- src/v1/controllers/follow.js
+++ src/v1/controllers/follow.js
@@ -11,13 +11,8 @@ async function newUser(req, res, next) {
   try {
     const token = req.headers.authorization.split(' ')[1];
     const userObj = await jwt.resolve(token);
-    console.log('userObj: ', userObj);
     const userProfile = await registrationModel.findOne({email: userObj.email});
-    console.log('userProfile: ', userProfile);
-    console.log(req.body);
-    console.log('req.body.username: ', req.body.username);
     const followUser = await registrationModel.findOne({username: req.body.username});
-    console.log('followUser: ', followUser);
     if (userObj && userProfile && userProfile.password === userObj.password && followUser) {
       await registrationModel.findOneAndUpdate({email: userProfile.email},
           {$push: {following: followUser.username}, $set: {followingCount: userProfile.following.length + 1}},
