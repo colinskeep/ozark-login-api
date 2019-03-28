c8024ddaeeb954961e71e5e751772a2bea76c207
colinskeep <colinjinskeep@gmail.com>
2019-03-28 02:28:16 -0700

    refactor

--- src/v1/controllers/verify.js
+++ src/v1/controllers/verify.js
@@ -29,6 +29,7 @@ async function postValidate(req, res, next) {
         id: verifyCode.id,
         name: verifyCode.name,
         username: verifyCode.username,
+        thumbnail: pfpic.thumbnail,
       });
     } else {
       res.status(200).json({data: false});
